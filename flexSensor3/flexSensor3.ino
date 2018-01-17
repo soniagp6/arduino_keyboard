c27b1ec7e958cd8e1f4fa1ffc2c81de3b0348ddf
SONIA PUTZEL <sonia.putzel@ogilvy.com>
2018-01-17 16:25:57 -0500

    removed serial stuff and added another finger in to test

--- flexSensor3/flexSensor3.ino
+++ flexSensor3/flexSensor3.ino
@@ -11,7 +11,6 @@
 #define FACTORYRESET_ENABLE         0
 #define MINIMUM_FIRMWARE_VERSION    "0.6.6"
 
-
 bool keyboardOn = false;
 int ledPin = 12;
 int inputPin = 11;
@@ -22,15 +21,15 @@ Adafruit_BluefruitLE_SPI ble(8, 7, 4);
 
 
 //Finger finger0(5, true);
-//Finger finger1(4, true, 0, 0);
-//Finger finger2(3, true, 650, 350);
-//Finger finger3(2, true, 670, 460);
-//Finger finger4(1, true, 600, 380);
+//Finger finger1(4, true, 0, 0, &ble);
+//Finger finger2(3, true, 650, 350, &ble);
+//Finger finger3(2, true, 670, 460, &ble);
+//Finger finger4(1, true, 600, 380, &ble);
 
-//Finger finger0(5, false, 0, 0);
-//Finger finger1(4, false, 490, 150);
-//Finger finger2(3, false, 650, 350);
-//Finger finger3(2, false, 600, 320);
+//Finger finger0(5, false, 0, 0, &ble);
+//Finger finger1(4, false, 490, 150, &ble);
+//Finger finger2(3, false, 650, 350, &ble);
+Finger finger3(2, false, 600, 320, &ble);
 Finger finger4(1, false, 680, 410, &ble);
 
 #define VERBOSE_MODE  false
@@ -40,62 +39,62 @@ void setup(void)
   pinMode(inputPin, INPUT);
   pinMode(ledPin, OUTPUT);
   digitalWrite(inputPin, HIGH);
-  while (!Serial);  // required for Flora & Micro
-  delay(500);
-
-  Serial.begin(115200);
-  Serial.println(F("Adafruit Bluefruit HID Keyboard Example"));
-  Serial.println(F("---------------------------------------"));
+//  while (!Serial);  // required for Flora & Micro
+//  delay(500);
+//
+//  Serial.begin(115200);
+//  Serial.println(F("Adafruit Bluefruit HID Keyboard Example"));
+//  Serial.println(F("---------------------------------------"));
 
   /* Initialise the module */
-  Serial.print(F("Initialising the Bluefruit LE module: "));
+//  Serial.print(F("Initialising the Bluefruit LE module: "));
 
   if ( !ble.begin(VERBOSE_MODE) )
   {
-    error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
+    //error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
   }
-  Serial.println( F("OK!") );
+  //Serial.println( F("OK!") );
 
   if ( FACTORYRESET_ENABLE )
   {
     /* Perform a factory reset to make sure everything is in a known state */
-    Serial.println(F("Performing a factory reset: "));
+    //Serial.println(F("Performing a factory reset: "));
     if ( ! ble.factoryReset() ){
-      error(F("Couldn't factory reset"));
+      //error(F("Couldn't factory reset"));
     }
   }
   
   /* Disable command echo from Bluefruit */
   ble.echo(false);
 
-  Serial.println("Requesting Bluefruit info:");
+  //Serial.println("Requesting Bluefruit info:");
   /* Print Bluefruit information */
   ble.info();
 
   /* Change the device name to make it easier to find */
-  Serial.println(F("Setting device name to 'Bluefruit Keyboard': "));
+  //Serial.println(F("Setting device name to 'Bluefruit Keyboard': "));
   if (! ble.sendCommandCheckOK(F( "AT+GAPDEVNAME=Bluefruit Keyboard" )) ) {
     error(F("Could not set device name?"));
   }
 
   /* Enable HID Service */
-  Serial.println(F("Enable HID Service (including Keyboard): "));
+  //Serial.println(F("Enable HID Service (including Keyboard): "));
   if ( ble.isVersionAtLeast(MINIMUM_FIRMWARE_VERSION) )
   {
     if ( !ble.sendCommandCheckOK(F( "AT+BleHIDEn=On" ))) {
-      error(F("Could not enable Keyboard"));
+      //error(F("Could not enable Keyboard"));
     }
   }else
   {
     if (! ble.sendCommandCheckOK(F( "AT+BleKeyboardEn=On"  ))) {
-      error(F("Could not enable Keyboard"));
+      //error(F("Could not enable Keyboard"));
     }
   }
 
   /* Add or remove service requires a reset */
-  Serial.println(F("Performing a SW reset (service changes require a reset): "));
+  //Serial.println(F("Performing a SW reset (service changes require a reset): "));
   if (! ble.reset() ) {
-    error(F("Couldn't reset??"));
+    //error(F("Couldn't reset??"));
   }
 }
 
@@ -165,8 +164,3 @@ void error(const __FlashStringHelper*err) {
   Serial.println(err);
   while (1);
 }
-
-
-
-
-
