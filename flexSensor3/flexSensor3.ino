e27377b6adfb6c2faccdad1b634e4f63e7f94fb0
SONIA PUTZEL <sonia.putzel@ogilvy.com>
2018-01-17 16:13:08 -0500

    removed references to keyboard library in sketch file

--- flexSensor3/flexSensor3.ino
+++ flexSensor3/flexSensor3.ino
@@ -1,56 +1,45 @@
 #include <Arduino.h>
+#include <Finger.h>
 
-// For Bluetooth
 #include <SPI.h>
 #include "Adafruit_BLE.h"
 #include "Adafruit_BluefruitLE_SPI.h"
 #include "Adafruit_BluefruitLE_UART.h"
+
 #include "BluefruitConfig.h"
-
-#if SOFTWARE_SERIAL_AVAILABLE
-  #include <SoftwareSerial.h>
-#endif
-
-#include <Keyboard.h>
-#include <Finger.h>
-
 
 #define FACTORYRESET_ENABLE         0
 #define MINIMUM_FIRMWARE_VERSION    "0.6.6"
 
-/* ...hardware SPI, using SCK/MOSI/MISO hardware SPI pins and then user selected CS/IRQ/RST */
-Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
 
 bool keyboardOn = false;
 int ledPin = 12;
 int inputPin = 11;
 int buttonState = 0;
 
+/* ...hardware SPI, using SCK/MOSI/MISO hardware SPI pins and then user selected CS/IRQ/RST */
+Adafruit_BluefruitLE_SPI ble(8, 7, 4);
+
+
 //Finger finger0(5, true);
-Finger finger1(4, true, 0, 0);
-Finger finger2(3, true, 650, 350);
-Finger finger3(2, true, 670, 460);
-Finger finger4(1, true, 600, 380);
+//Finger finger1(4, true, 0, 0);
+//Finger finger2(3, true, 650, 350);
+//Finger finger3(2, true, 670, 460);
+//Finger finger4(1, true, 600, 380);
 
 //Finger finger0(5, false, 0, 0);
 //Finger finger1(4, false, 490, 150);
 //Finger finger2(3, false, 650, 350);
 //Finger finger3(2, false, 600, 320);
-//Finger finger4(1, false, 680, 410);
+Finger finger4(1, false, 680, 410, &ble);
 
+#define VERBOSE_MODE  false
 
-// A small helper
-void error(const __FlashStringHelper*err) {
-  Serial.println(err);
-  while (1);
-}
-
-void setup()
+void setup(void)
 {
   pinMode(inputPin, INPUT);
   pinMode(ledPin, OUTPUT);
   digitalWrite(inputPin, HIGH);
-  Keyboard.begin();
   while (!Serial);  // required for Flora & Micro
   delay(500);
 
@@ -75,7 +64,7 @@ void setup()
       error(F("Couldn't factory reset"));
     }
   }
-
+  
   /* Disable command echo from Bluefruit */
   ble.echo(false);
 
@@ -108,19 +97,6 @@ void setup()
   if (! ble.reset() ) {
     error(F("Couldn't reset??"));
   }
-
-  Serial.println();
-  Serial.println(F("Go to your phone's Bluetooth settings to pair your device"));
-  Serial.println(F("then open an application that accepts keyboard input"));
-
-  Serial.println();
-  Serial.println(F("Enter the character(s) to send:"));
-  Serial.println(F("- \\r for Enter"));
-  Serial.println(F("- \\n for newline"));
-  Serial.println(F("- \\t for tab"));
-  Serial.println(F("- \\b for backspace"));
-
-  Serial.println();
 }
 
 void loop() {
@@ -167,63 +143,30 @@ void loop() {
       //Range - 680 to 410
       //NOPQ
       finger4.onLoop();
-
-
+     
       //Thumb
-      //finger5.onLoop();
-      
-  }
-
-  Serial.print(F("keyboard > "));
-
-  // Check for user input and echo it back if anything was found
-  char keys[BUFSIZE+1];
-  getUserInput(keys, BUFSIZE);
-
-  Serial.print("\nSending ");
-  Serial.println(keys);
-
-  ble.print("AT+BleKeyboard=");
-  ble.println(keys);
-
-  if( ble.waitForOK() )
-  {
-    Serial.println( F("OK!") );
-  }else
-  {
-    Serial.println( F("FAILED!") );
+      //finger5.onLoop();  
   }
 }
 
 void checkOnButton() {
   buttonState = digitalRead(inputPin);
-    if (buttonState == HIGH) {
-        digitalWrite(ledPin, LOW);
-        keyboardOn = false;
-    } else {
-        digitalWrite(ledPin, HIGH);
-        keyboardOn = true;
-    }
-}
-
-/**************************************************************************/
-/*!
-    @brief  Checks for user input (via the Serial Monitor)
-*/
-/**************************************************************************/
-void getUserInput(char buffer[], uint8_t maxSize)
-{
-  memset(buffer, 0, maxSize);
-  while( Serial.available() == 0 ) {
-    delay(1);
+  if (buttonState == HIGH) {
+    digitalWrite(ledPin, LOW);
+    keyboardOn = false;
+  } else {
+    digitalWrite(ledPin, HIGH);
+    keyboardOn = true;
   }
-
-  uint8_t count=0;
-
-  do
-  {
-    count += Serial.readBytes(buffer+count, maxSize);
-    delay(2);
-  } while( (count < maxSize) && !(Serial.available() == 0) );
 }
+
+// A small helper
+void error(const __FlashStringHelper*err) {
+  Serial.println(err);
+  while (1);
+}
+
+
+
+
 
